a806859309c65ef3b6df7ad8e659bd7a40547855
busylee999 <demonlee999@gmail.com>
2013-11-08 14:04:59 +0400

    pong message implementation

--- TorChatAndroid/src/prof7bit/torchat/core/Msg_pong.java
+++ TorChatAndroid/src/prof7bit/torchat/core/Msg_pong.java
@@ -1,26 +1,48 @@
 package prof7bit.torchat.core;
 
+import java.io.EOFException;
+
 /**
  * This class handles the protocol message "pong".
- *
+ * 
+ * ****************************** 
+ * "pong" packet example: 
+ * pong 42610670386469110587786764749331231960 
+ *      <random-string>
+ * ******************************
+ * 
  * @author Bernd Kreuss <prof7bit@gmail.com>
  *
  */
 public class Msg_pong extends Msg {
 
+	final static String MSG_COMMAND = "pong";
+	String mRandomString = null;
+	
 	public Msg_pong(Connection connection) {
 		super(connection);
+		mCommand = MSG_COMMAND;
 	}
 
 	@Override
 	public void parse(MessageBuffer buf) throws XMessageParseException {
-		// TODO Auto-generated method stub
+		try{
+			//read random string
+			mRandomString = buf.readString();
+		} catch (EOFException e){
+			throw new XMessageParseException(e.getLocalizedMessage());
+		}
 	}
 
 	@Override
 	public MessageBuffer serialize() {
 		MessageBuffer mb = new MessageBuffer();
-		// TODO Auto-generated method stub
+		
+		//write command first
+		writeCommand(mb);
+		
+		writeRandomString(mb);
+		
 		return mb;
 	}
 
@@ -28,6 +50,27 @@ public class Msg_pong extends Msg {
 	public void execute() {
 		System.out.println("Msg_pong.execute()");
 		// TODO Auto-generated method stub
+	}
+	
+	/**
+	 * write random string into passed buffer
+	 * 
+	 * @param buffer
+	 */
+	protected void writeRandomString(MessageBuffer buffer) {
+		if (mRandomString != null)
+			buffer.writeString(mRandomString);
+	}
+	
+	/*********************
+	 * GETTERS AND SETTERS
+	 ********************/
+	public String getRandomString() {
+		return mRandomString;
+	}
+
+	public void setRandomString(String mRandomString) {
+		this.mRandomString = mRandomString;
 	}
 
 }
